ad3cb2d4d6563404d992bf67d8c95980924ba5af
Kugesz <kovacsgergo.algyo@gmail.com>
2023-10-17 09:22:45 +0200

    kesz

---
+++ javito.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+
+document.getElementsByClassName("container")[0].style.setProperty("background-color", "#12406A");
+
+
+document.getElementById("tarolo1").style.setProperty("background-color", "#EB8F90");
+document.getElementById("tarolo2").style.setProperty("background-color", "#FFB471");
+document.getElementById("tarolo3").style.setProperty("background-color", "#ADBED2");
+
+document.body.style.setProperty("background-color", "#CCFFFF");
+
+}
